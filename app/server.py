b06ac294ca4f0564d6fc727f2a737e4a1ffadca5
Khanh Huynh <khanh.huynh@knorex.com>
2019-07-18 06:29:32 +0700

    Change `export_file_url` and `classes`

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1GgbsY-s1O9UvTyiO0rp1_MQej4dT6R2E'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['baseball', 'golf', 'soccer', 'tennis']
 path = Path(__file__).parent
 
 app = Starlette()
